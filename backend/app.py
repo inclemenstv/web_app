e75436b0d6b69c245224a139bf61547560a034a9
Your Name <you@example.com>
2021-06-28 14:40:38 +0300

    change backend

--- backend/app.py
+++
@@ -1,6 +0,0 @@
-import os
-from flask import Flask
-from flask_cors import CORS, cross_origin
-
-app = Flask(__name__)
-CORS(app)
--- backend/main.py
+++ backend/main.py
@@ -1,135 +1,85 @@
-import pymysql
-from app import app
-from db import mysql
-from flask import jsonify
-from flask import flash, request
+import os
+from flask import jsonify, request, Flask
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
+app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
+app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
+app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
+mysql.init_app(app)
 
 
 @app.route("/")
 def index():
     """Function to test the functionality of the API"""
-    return "Web-app"
+    return "Hello, world!"
 
 
-@app.route('/api/v1/add', methods=['POST'])
+@app.route("/api/v1/add", methods=["POST"])
 def add_user():
-    try:
-        _json = request.json
-        _name = _json['name']
-        _email = _json['email']
-        # validate the received values
-        if _name and _email and request.method == 'POST':
-            # save edits
-            sql = "INSERT INTO users(user_name, user_email) VALUES(%s, %s)"
-            data = (_name, _email,)
+    """Function to create a user to the MySQL database"""
+    json = request.json
+    name = json["name"]
+    email = json["email"]
+    if name and email and request.method == "POST":
+        sql = "INSERT INTO users(user_name, user_email) " \
+              "VALUES(%s, %s)"
+        data = (name, email)
+        try:
             conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sql, data)
             conn.commit()
-            resp = jsonify('User added successfully!')
+            cursor.close()
+            conn.close()
+            resp = jsonify("User created successfully!")
             resp.status_code = 200
             return resp
-        else:
-            return not_found()
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close()
-        conn.close()
+        except Exception as exception:
+            return jsonify(str(exception))
+    else:
+        return jsonify("Please provide name, email")
 
 
-@app.route('/api/v1/users', methods=["GET"])
+@app.route("/api/v1/users", methods=["GET"])
 def users():
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT user_id id, user_name name, user_email email FROM users")
-        rows = cursor.fetchall()
-        resp = jsonify(rows)
-        resp.status_code = 200
-        return resp
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close()
-        conn.close()
-
-
-@app.route('/api/v1/user/<int:id>', methods=["GET"])
-def user(id):
-    try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute(
-            "SELECT user_id id, user_name name, user_email email FROM users WHERE user_id=%s", id)
-        row = cursor.fetchone()
-        resp = jsonify(row)
-        resp.status_code = 200
-        return resp
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close()
-        conn.close()
-
-
-@app.route('/api/v1/update', methods=['PUT'])
-def update_user():
-    try:
-        _json = request.json
-        _id = _json['id']
-        _name = _json['name']
-        _email = _json['email']
-        # validate the received values
-        if _name and _email and _id and request.method == 'PUT':
-            # save edits
-            sql = "UPDATE users SET user_name=%s, user_email=%s WHERE user_id=%s"
-            data = (_name, _email, _id,)
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute(sql, data)
-            conn.commit()
-            resp = jsonify('User updated successfully!')
-            resp.status_code = 200
-            return resp
-        else:
-            return not_found()
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close()
-        conn.close()
-
-
-@app.route('/api/v1/delete/<int:id>', methods=['GET', 'POST', 'DELETE'])
-def delete_user(id):
+    """Function to retrieve all users from the MySQL database"""
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM users WHERE user_id=%s", id)
-        conn.commit()
-        resp = jsonify('User deleted successfully!')
-        resp.status_code = 200
-        return resp
-    except Exception as e:
-        print(e)
-    finally:
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
         cursor.close()
         conn.close()
+        resp = jsonify(rows)
+        resp.status_code = 200
+        return resp
+    except Exception as exception:
+        return jsonify(str(exception))
 
 
-@app.errorhandler(404)
-def not_found(error=None):
-    message = {
-        'status': 404,
-        'message': 'Not Found: ' + request.url,
-    }
-    resp = jsonify(message)
-    resp.status_code = 404
-
-    return resp
+@app.route("/api/v1/delete/<int:user_id>")
+def delete_user(user_id):
+    """Function to delete a user from the MySQL database"""
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM users WHERE user_id=%s", user_id)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        resp = jsonify("User deleted successfully!")
+        resp.status_code = 200
+        return resp
+    except Exception as exception:
+        return jsonify(str(exception))
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
